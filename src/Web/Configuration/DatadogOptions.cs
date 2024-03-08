e79e5d13317badeaaddd81ab6245585b76f7853f
pstephenson02 <phil.m.stephenson@datadoghq.com>
2024-03-08 08:37:38 -0800

    Define the trace agent port for local. Our local agent config was recently changed by Enterprise IT: https://github.com/DataDog/h2o/pull/511

--- src/Web/Configuration/DatadogOptions.cs
+++ src/Web/Configuration/DatadogOptions.cs
@@ -6,6 +6,7 @@ public class DatadogOptions
 
     public string Site { get; set; } = string.Empty;
     public string Service { get; set; } = string.Empty;
+    // When running locally: https://learn.microsoft.com/en-us/aspnet/core/security/app-secrets
     public string ClientToken { get; set; } = string.Empty;
     public string ApplicationId { get; set; } = string.Empty;
 }
